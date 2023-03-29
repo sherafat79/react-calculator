cff6df9d951176485c9d4de38973617927365d88
mohammad mahdi sherafat <mohammadmahdisherafat79@gmail.com>
2023-03-29 11:08:18 +0330

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,51 @@
-import { useEffect, useState } from "react"
-import Calculator from "./components/Calculator"
-import Container from "./components/Container"
-import Header from "./components/Header"
-import SideBar from "./components/SideBar"
-import Wrapper from "./components/Wrapper"
-import AppContext from "./context/AppContext"
-import { IResult } from "./types/interfaces"
-import { getDarkMode, getResults } from "./utils/localStorage"
-
+import { useEffect, useState } from "react";
+import Calculator from "./components/Calculator";
+import Container from "./components/Container";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import Wrapper from "./components/Wrapper";
+import AppContext from "./context/AppContext";
+import { IResult } from "./types/interfaces";
+import { getDarkMode, getResults } from "./utils/localStorage";
 
 const App = () => {
-  const [results, setResults] = useState<Array<IResult>>(getResults)
-  const [darkMode, setDarkMode] = useState<boolean>(getDarkMode)
+  const [results, setResults] = useState<Array<IResult>>(getResults);
+  const [darkMode, setDarkMode] = useState<boolean>(getDarkMode);
   const insertResult = (result: IResult) => {
     setResults((preveState) => [...preveState, result]);
-    localStorage.setItem("results", JSON.stringify([...results, result]))
-  }
+    localStorage.setItem("results", JSON.stringify([...results, result]));
+  };
 
   const clearResult = () => {
-    setResults([])
-    localStorage.setItem("results", JSON.stringify([]))
-  }
+    setResults([]);
+    localStorage.setItem("results", JSON.stringify([]));
+  };
   const darkModeHandler = () => {
-    setDarkMode(d => !d);
-    localStorage.setItem("darkMode", (!darkMode).toString())
-  }
+    setDarkMode((d) => !d);
+    localStorage.setItem("darkMode", (!darkMode).toString());
+  };
   return (
     <Wrapper isDark={darkMode}>
-      <AppContext.Provider value={{
-        insertResult,
-        clearResult,
-        darkMode,
-        darkModeHandler,
-        results
-      }}>
+      <AppContext.Provider
+        value={{
+          insertResult,
+          clearResult,
+          darkMode,
+          darkModeHandler,
+          results,
+        }}
+      >
         <Header />
 
         <div className=" flex-auto flex dark:text-white dark:bg-slate-900">
           <SideBar />
-          <Container >
+          <Container>
             <Calculator />
           </Container>
         </div>
       </AppContext.Provider>
     </Wrapper>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
